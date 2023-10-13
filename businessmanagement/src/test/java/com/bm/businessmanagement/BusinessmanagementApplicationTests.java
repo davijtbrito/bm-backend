5dce387cd06f486edbbb2fd3e7b681356f786f95
davibrito1218 <davibrito1218@gmail.com>
2023-10-13 19:03:09 +0100

    feat: initial commit

---
+++ businessmanagement/src/test/java/com/bm/businessmanagement/BusinessmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bm.businessmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
